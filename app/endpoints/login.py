0c48cca3b2db1f729a164702fe2d9cbe7156bfb7
Oluwatooki <oluwatooki@gmail.com>
2024-08-28 00:26:37 +0100

    feat: use psycopg2.sql.SQL for safer queries and ensure connection closure
    - Refactored SQL query to use psycopg2.sql.SQL, sql.Identifier, and sql.Placeholder.
    - Added finally block to close the database connection properly.

--- app/endpoints/login.py
+++ app/endpoints/login.py
@@ -1,44 +1,65 @@
 import logging
+import psycopg2
 from fastapi import APIRouter, status, HTTPException, Depends, Response, Request
 from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from app.schemas import user_schemas
 from app.db import connection
 from app.core import utils
 from app.core import security
+from psycopg2 import sql
 
 router = APIRouter(tags=['Login'])
 logger = logging.getLogger(__name__)
 cred_error = 'Invalid Credentials'
 
 
-@router.post("/login",
-             summary="Logs in a user, returns a jwt token",
-             # response_model=user_schemas.Token,
-             status_code=status.HTTP_202_ACCEPTED)
-async def login(user_credentials: OAuth2PasswordRequestForm = Depends(),
-                database_access: list = Depends(connection.get_db)
-                ):
+@router.post(
+    "/register",
+    status_code=status.HTTP_201_CREATED,
+    summary='Creating a new user in the database',
+    response_model=user_schemas.UserOut
+)
+async def create_user(
+        user_data: user_schemas.UserCreate,
+        database_access: list = Depends(connection.get_db)):
     conn, cursor = database_access
-    # OAuth2Password Request Form Returns username and password as a dictionary,
-    # in this case the username is the email
-    user_email = str(user_credentials.username)
 
-    cursor.execute(f"SELECT * FROM users WHERE email = %s ", (user_email,))
-    user = cursor.fetchone()
+    user_data.password = await utils.bcrypt_hash(user_data.password)
 
-    if not user:
-        logger.error(cred_error)
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=cred_error)
-    if not await utils.verify_login_details(user_credentials.password, user['password']):
-        logger.error(cred_error)
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=cred_error)
-    access_token = await security.create_access_token(data={'user_id': user['user_id']})
-    logger.info(f'User {user['user_id']} logged in')
-    user = user_schemas.UserOut(user_id=user['user_id'], email=user['email'], created_date=user['created_date'],
-                                first_name=user['first_name'], last_name=user['last_name'])
+    try:
+        user_data = user_data.model_dump()
 
-    token = user_schemas.Token(access_token=access_token, token_type="bearer")
-    return user_schemas.Login(user=user, token=token)
+        insert_query = sql.SQL("""
+            INSERT INTO {table} ({fields})
+            VALUES ({values})
+            RETURNING *;
+        """).format(
+            table=sql.Identifier('users'),
+            fields=sql.SQL(', ').join([
+                sql.Identifier('email'),
+                sql.Identifier('first_name'),
+                sql.Identifier('last_name'),
+                sql.Identifier('password')
+            ]),
+            values=sql.SQL(', ').join([sql.Placeholder(k) for k in user_data.keys()])
+        )
+
+        cursor.execute(insert_query, user_data)
+        new_user = cursor.fetchone()
+
+    except psycopg2.errors.UniqueViolation as error:
+        logger.warning(f"Attempt to create user with duplicate email: {user_data['email']}")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail="Email already in use")
+    except Exception as e:
+        logger.error(f"Error occurred while creating user: {str(e)}", exc_info=True)
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+    else:
+        conn.commit()
+        logger.info(f"User created successfully: {new_user}")
+        return new_user
+    finally:
+        conn.close()
 
 
 @router.get('/me',
